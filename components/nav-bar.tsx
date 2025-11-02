eb9fa7d6456fb7e2c1501c2295c6cbc1a7bc0ca4
Akbar Tokochev <atokochev@gmail.com>
2025-11-02 21:05:32 +0300

    layout (navbar): center elements

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -15,7 +15,7 @@ const links = [
     name: "Education",
   },
   {
-    href: "#",
+    href: "#experience",
     name: "Experience",
   },
 ];
@@ -24,19 +24,16 @@ export default function NavBar() {
   return (
     <div className="mt-2 fixed w-full z-20">
       <div className="py-4 px-6 relative max-w-5xl mx-auto z-10 border rounded-2xl bg-background/50 backdrop-blur-lg">
-        <div className="flex justify-between items-center">
-          <div></div>
-          <div className="flex gap-8 text-sm text-muted-foreground">
-            {links.map((link) => (
-              <Link
-                key={link.name}
-                href={link.href}
-                className="hover:text-foreground"
-              >
-                {link.name}
-              </Link>
-            ))}
-          </div>
+        <div className="flex justify-center items-center gap-8 text-sm text-muted-foreground">
+          {links.map((link) => (
+            <Link
+              key={link.name}
+              href={link.href}
+              className="hover:text-foreground"
+            >
+              {link.name}
+            </Link>
+          ))}
           <ThemeToggle />
         </div>
       </div>
