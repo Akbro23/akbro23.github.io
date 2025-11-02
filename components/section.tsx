29d0a653f1a352cc65cd625a2ecbea2d788a24d9
Akbar Tokochev <atokochev@gmail.com>
2025-11-02 18:06:57 +0300

    fix (section): gradient fix

--- components/section.tsx
+++ components/section.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function Section({children} : {children: ReactNode}) {
   return (
     <div className="grid grid-cols-12">
-      <div className="col-span-10 col-start-2 bg-linear-to-br from-background to-background-light rounded-2xl py-16 z-0 border shadow hover:shadow-lg hover:from-background-light transition">
+      <div className="col-span-10 col-start-2 bg-linear-to-br from-background-light to-background rounded-2xl py-16 z-0 border shadow hover:shadow-lg hover:to-background-light transition">
         {children}
       </div>
     </div>
